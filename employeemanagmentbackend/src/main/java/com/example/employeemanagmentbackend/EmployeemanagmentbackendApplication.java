61c5a3a44d50846b0fee5720e5b58022a561d994
moha1747 <mohamud.abbaas14@gmail.com>
2024-04-14 21:35:04 -0500

    add backend folder

---
+++ employeemanagmentbackend/src/main/java/com/example/employeemanagmentbackend/EmployeemanagmentbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagmentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagmentbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagmentbackendApplication.class, args);
+	}
+
+}
