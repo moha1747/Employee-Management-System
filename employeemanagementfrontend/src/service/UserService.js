56110cbcdb322e6cfdd3c6b6597531e20fe596c9
moha1747 <mohamud.abbaas14@gmail.com>
2024-04-29 00:57:59 -0500

    fix user defined routes

--- employeemanagementfrontend/src/service/UserService.js
+++ employeemanagementfrontend/src/service/UserService.js
@@ -9,7 +9,7 @@ class UserService {
     return axios.delete(`${BASE_URL}/${id}`);
   }
   /**Method to get all employee from our api or database */
-  getAllEmplpoyee(id, employeeData) {
+  getAllEmployee(id, employeeData) {
     return axios.get(`${BASE_URL}/${id}/employees`, employeeData);
   }
     getEmployeeById(id, employeeData, employeeId) {
@@ -34,29 +34,3 @@ class UserService {
   }
 }
 export default new UserService();
-// // import axios from "axios";
-
-// const BASEURL = "http://localhost:8080/employee";
-// class EmployeeService {
-//   //**Method to get all employee from our api or database */
-//   getAllEmployee() {
-//     return axios.get(BASE_URL);
-//   }
-//   /**MEthod to save employee */
-//   addEmployeeToUser(id, employeeData) {
-//     return axios.post(
-//       `http://localhost:8080/user/${id}/employee`, id,
-//       employeeData
-//     );
-//   }
-//   updateEmployee(id, employeeData) {
-//     return axios.put(`${BASE_URL}/${id}`, employeeData);
-//   }
-//   getEmployeeById(id) {
-//     return axios.get(`${BASE_URL}/${id}`);
-//   }
-//   deleteEmployee(id) {
-//     return axios.delete(`${BASE_URL}/${id}`);
-//   }
-// }
-// export default new EmployeeService();
